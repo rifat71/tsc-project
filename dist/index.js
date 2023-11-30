a189c6a91a8b21acece937fb3b43609fde51ef38
rifat71 <rifatmahmud24311@gmail.com>
2023-11-30 22:01:32 +0600

    tuple

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
 let age = 20;
+if (age < 50)
+    age += 20;
 console.log(age);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 let age: number = 20;
-
+if(age <50)
+ age+=20;
+let array: number[] = [1,2,3];
+let usear: [number, string] = [1, 'galib'];  //tuple
 console.log(age)
-  
+
